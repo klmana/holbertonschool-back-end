6719f0855c8ca63529181fe8caf161392e0ee7ab
klmana <104742971+klmana@users.noreply.github.com>
2022-12-02 09:50:59 +1000

    fix error and add format need it

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,29 +7,34 @@
 import requests
 from sys import argv
 
+
+def counter(completed=None):
+    """
+      Just to count completed task
+    """
+
+    count = 0
+    for any in todo:
+        if any.get('completed') is True:
+            count += 1
+    return count
+
+
 if __name__ == "__main__":
 
-    def counter(completed=None):
-        """
-          Just to count completed task
-        """
-        count = 0
-        for any in todo:
-            if any.get('completed') is True:
-                count += 1
-        return count
+    payload = {'id': argv[1]}
+    user = requests.get('https://jsonplaceholder.typicode.com/users'.format,
+                        params=payload).json()
 
-        payload = {'id': argv[1]}
-        user = requests.get('https://jsonplaceholder.typicode.com/users',
-                            params=payload).json()
+    payload2 = {'userId': argv[1]}
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos'.format,
+                        params=payload2).json()
 
-        payload2 = {'userId': argv[1]}
-        todo = requests.get('https://jsonplaceholder.typicode.com/todos',
-                            params=payload2).json()
+    print('Employee {} is done with tasks({}/{}):'.format(
+     user[0].get('name'),
+     counter(todo),
+     len(todo)))
 
-        print('Employee {} is done with tasks({}/{}):'.format(
-            user[0].get('name'), counter(todo), len(todo)))
-
-        for any in todo:
-            if any.get('completed') is True:
-                print("\t {}".format(any.get('title')))
+    for any in todo:
+        if any.get('completed') is True:
+            print("\t {}".format(any.get('title')))
