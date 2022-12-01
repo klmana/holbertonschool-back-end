8868a914d415e8c504e85f94746c6d679d0c3f47
klmana <104742971+klmana@users.noreply.github.com>
2022-12-01 18:54:26 +1000

    task0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+  Script that, using this REST API, for a given employee ID,
+  returns information about his/her TODO list progress.
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    def counter(completed=None):
+        """
+          Just to count completed task
+        """
+        count = 0
+        for any in todo:
+            if any.get('completed') is True:
+                count += 1
+        return count
+
+        payload = {'id': argv[1]}
+        user = requests.get('https://jsonplaceholder.typicode.com/users',
+                            params=payload).json()
+
+        payload2 = {'userId': argv[1]}
+        todo = requests.get('https://jsonplaceholder.typicode.com/todos',
+                            params=payload2).json()
+
+        print('Employee {} is done with tasks({}/{}):'.format(
+            user[0].get('name'), counter(todo), len(todo)))
+
+        for any in todo:
+            if any.get('completed') is True:
+                print("\t {}".format(any.get('title')))
