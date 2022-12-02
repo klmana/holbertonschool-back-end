bb5b6944db56f94162151eda72ee402f37756211
klmana <104742971+klmana@users.noreply.github.com>
2022-12-02 10:07:54 +1000

    fix error and add format need it

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,9 +9,7 @@ from sys import argv
 
 
 def counter(completed=None):
-    """
-      Just to count completed task
-    """
+    """counter of completed tasks"""
 
     count = 0
     for arg in todo:
@@ -19,21 +17,20 @@ def counter(completed=None):
             count += 1
     return count
 
-
 if __name__ == "__main__":
 
     payload = {'id': argv[1]}
-    user = requests.get('https://jsonplaceholder.typicode.com/users'.format,
+    user = requests.get('https://jsonplaceholder.typicode.com/users',
                         params=payload).json()
 
     payload2 = {'userId': argv[1]}
-    todo = requests.get('https://jsonplaceholder.typicode.com/todos'.format,
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos',
                         params=payload2).json()
 
     print('Employee {} is done with tasks({}/{}):'.format(
-     user[0].get('name'),
-     counter(todo),
-     len(todo)))
+        user[0].get('name'),
+        counter(todo),
+        len(todo)))
 
     for arg in todo:
         if arg.get('completed') is True:
